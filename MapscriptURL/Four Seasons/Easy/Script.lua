5636bf88778cdc4f00e9fc03ba4e47ab84aaf2a2
Tseting-nil <chenjustin399.5@gmail.com>
2025-07-30 12:08:42 +0800

    測試

---
+++ MapscriptURL/Four Seasons/Easy/Script.lua
@@ -0,0 +1,21 @@
+local Script = {
+    {
+    Name = "嘻嘻哈哈 - 1",
+    Title = "Four Seasons (測試1)(Tseting-nil)",
+    Author = "Tseting-nil",
+    Time = "2025/07/30",
+    Tower = {
+        "Farm","Soldier","Medic","DJ Booth","ACC"},--ACC = Accelerator
+    Script = " "
+    },
+    {
+    Name = "嘻嘻哈哈 - 2",
+    Title = "Four Seasons (測試2)(Tseting-nil)",
+    Author = "Tseting-nil",
+    Time = "2025/07/30",
+    Tower = {
+        "Farm","Soldier","Medic","DJ Booth","ACC"},--ACC = Accelerator
+    Script = " "
+    },
+}
+return Script
