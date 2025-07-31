c17b345f920b8530f4179d3586bb27d176bd007b
Tseting-nil <chenjustin399.5@gmail.com>
2025-07-31 15:44:48 +0800

    測試3

--- MapscriptURL/Four Seasons/Easy/Script.lua
+++ MapscriptURL/Four Seasons/Easy/Script.lua
@@ -3,7 +3,7 @@ local Script = {
     Name = "嘻嘻哈哈 - 1",
     Title = "Four Seasons (測試1)(Tseting-nil)",
     Author = "Tseting-nil",
-    Time = "2025/07/30",
+    Time = "12:00",
     Tower = {
         "Farm","Soldier","Medic","DJ Booth","ACC"},--ACC = Accelerator
     Script = "https://raw.githubusercontent.com/Tseting-nil/Tower-Defense-Simulator/refs/heads/main/MapscriptURL/Four%20Seasons/Easy/Four_Seasons_1.lua"
@@ -12,9 +12,9 @@ local Script = {
     Name = "嘻嘻哈哈 - 2",
     Title = "Four Seasons (測試2)(Tseting-nil)",
     Author = "Tseting-nil",
-    Time = "2025/07/30",
+    Time = "10:30",
     Tower = {
-        "Farm","Soldier","Medic","DJ Booth","ACC"},--ACC = Accelerator
+        "Farm","GoldSoldier","Medic","DJ Booth","ACC"},--ACC = Accelerator
     Script = "https://raw.githubusercontent.com/Tseting-nil/Tower-Defense-Simulator/refs/heads/main/MapscriptURL/Four%20Seasons/Easy/Four_Seasons_1.lua"
     },
 }
