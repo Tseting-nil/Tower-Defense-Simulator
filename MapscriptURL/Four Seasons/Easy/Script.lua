df6abcacac54d89a63a22f9bba4d886574c0290e
Tseting-nil <chenjustin399.5@gmail.com>
2025-07-31 15:50:15 +0800

    測試多選

--- MapscriptURL/Four Seasons/Easy/Script.lua
+++ MapscriptURL/Four Seasons/Easy/Script.lua
@@ -1,21 +1,107 @@
 local Script = {
-    {
-    Name = "嘻嘻哈哈 - 1",
-    Title = "Four Seasons (測試1)(Tseting-nil)",
-    Author = "Tseting-nil",
-    Time = "12:00",
-    Tower = {
-        "Farm","Soldier","Medic","DJ Booth","ACC"},--ACC = Accelerator
-    Script = "https://raw.githubusercontent.com/Tseting-nil/Tower-Defense-Simulator/refs/heads/main/MapscriptURL/Four%20Seasons/Easy/Four_Seasons_1.lua"
-    },
-    {
-    Name = "嘻嘻哈哈 - 2",
-    Title = "Four Seasons (測試2)(Tseting-nil)",
-    Author = "Tseting-nil",
-    Time = "10:30",
-    Tower = {
-        "Farm","GoldSoldier","Medic","DJ Booth","ACC"},--ACC = Accelerator
-    Script = "https://raw.githubusercontent.com/Tseting-nil/Tower-Defense-Simulator/refs/heads/main/MapscriptURL/Four%20Seasons/Easy/Four_Seasons_1.lua"
-    },
+	{
+		Name = "嘻嘻哈哈 - 1",
+		Title = "Four Seasons (測試1)(Tseting-nil)",
+		Author = "Tseting-nil",
+		Time = "12:00",
+		Tower = {"Farm","Soldier","Medic","DJ Booth","ACC"},--ACC = Accelerator
+		Script = "https://raw.githubusercontent.com/Tseting-nil/Tower-Defense-Simulator/refs/heads/main/MapscriptURL/Four%20Seasons/Easy/Four_Seasons_1.lua"
+	},
+	{
+		Name = "嘻嘻哈哈 - 2",
+		Title = "Four Seasons (測試2)(Tseting-nil)",
+		Author = "Tseting-nil",
+		Time = "10:30",
+		Tower = {"Farm","GoldSoldier","Medic","DJ Booth","ACC"},--ACC = Accelerator
+		Script = "https://raw.githubusercontent.com/Tseting-nil/Tower-Defense-Simulator/refs/heads/main/MapscriptURL/Four%20Seasons/Easy/Four_Seasons_1.lua"
+	},
+	{
+		Name = "嘻嘻哈哈 - 3",
+		Title = "Four Seasons (測試3)(Tseting-nil)",
+		Author = "Tseting-nil",
+		Time = "10:30",
+		Tower = {"Farm","GoldSoldier","Medic","DJ Booth","Accelerator"},--ACC = Accelerator
+		Script = "https://raw.githubusercontent.com/Tseting-nil/Tower-Defense-Simulator/refs/heads/main/MapscriptURL/Four%20Seasons/Easy/Four_Seasons_1.lua"
+	},
+	{
+		Name = "嘻嘻哈哈 - 4",
+		Title = "Four Seasons (測試4)(Tseting-nil)",
+		Author = "Tseting-nil",
+		Time = "10:30",
+		Tower = {"Farm","GoldSoldier","Medic","DJ Booth","Accelerator"},--ACC = Accelerator
+		Script = "https://raw.githubusercontent.com/Tseting-nil/Tower-Defense-Simulator/refs/heads/main/MapscriptURL/Four%20Seasons/Easy/Four_Seasons_1.lua"
+	},
+	{
+		Name = "嘻嘻哈哈 - 5",
+		Title = "Four Seasons (測試5)(Tseting-nil)",
+		Author = "Tseting-nil",
+		Time = "1111111",
+		Tower = {"Farm","GoldSoldier","Medic","DJ Booth","Accelerator"},--ACC = Accelerator
+		Script = "https://raw.githubusercontent.com/Tseting-nil/Tower-Defense-Simulator/refs/heads/main/MapscriptURL/Four%20Seasons/Easy/Four_Seasons_1.lua"
+	},
+	{
+		Name = "嘻嘻哈哈 - 6",
+		Title = "Four Seasons (測試6)(Tseting-nil)",
+		Author = "Tseting-nil",
+		Time = "1111111",
+		Tower = {"Farm","GoldSoldier","Medic","DJ Booth","Accelerator"},--ACC = Accelerator
+		Script = "https://raw.githubusercontent.com/Tseting-nil/Tower-Defense-Simulator/refs/heads/main/MapscriptURL/Four%20Seasons/Easy/Four_Seasons_1.lua"
+	},
+	{
+		Name = "嘻嘻哈哈 - 7",
+		Title = "Four Seasons (測試7)(Tseting-nil)",
+		Author = "Tseting-nil",
+		Time = "1111111",
+		Tower = {"Farm","GoldSoldier","Medic","DJ Booth","Accelerator"},--ACC = Accelerator
+		Script = "https://raw.githubusercontent.com/Tseting-nil/Tower-Defense-Simulator/refs/heads/main/MapscriptURL/Four%20Seasons/Easy/Four_Seasons_1.lua"
+	},
+	{
+		Name = "嘻嘻哈哈 - 8",
+		Title = "Four Seasons (測試8)(Tseting-nil)",
+		Author = "Tseting-nil",
+		Time = "1111111",
+		Tower = {"Farm","GoldSoldier","Medic","DJ Booth","Accelerator"},--ACC = Accelerator
+		Script = "https://raw.githubusercontent.com/Tseting-nil/Tower-Defense-Simulator/refs/heads/main/MapscriptURL/Four%20Seasons/Easy/Four_Seasons_1.lua"
+	},
+	{
+		Name = "嘻嘻哈哈 - 9",
+		Title = "Four Seasons (測試9)(Tseting-nil)",
+		Author = "Tseting-nil",
+		Time = "1111111",
+		Tower = {"Farm","GoldSoldier","Medic","DJ Booth","Accelerator"},--ACC = Accelerator
+		Script = "https://raw.githubusercontent.com/Tseting-nil/Tower-Defense-Simulator/refs/heads/main/MapscriptURL/Four%20Seasons/Easy/Four_Seasons_1.lua"
+	},
+	{
+		Name = "嘻嘻哈哈 - 10",
+		Title = "Four Seasons (測試10)(Tseting-nil)",
+		Author = "Tseting-nil",
+		Time = "1111111",
+		Tower = {"Farm","GoldSoldier","Medic","DJ Booth","Accelerator"},--ACC = Accelerator
+		Script = "https://raw.githubusercontent.com/Tseting-nil/Tower-Defense-Simulator/refs/heads/main/MapscriptURL/Four%20Seasons/Easy/Four_Seasons_1.lua"
+	},
+	{
+		Name = "嘻嘻哈哈 - 11",
+		Title = "Four Seasons (測試11)(Tseting-nil)",
+		Author = "Tseting-nil",
+		Time = "1111111",
+		Tower = {"Farm","GoldSoldier","Medic","DJ Booth","Accelerator"},--ACC = Accelerator
+		Script = "https://raw.githubusercontent.com/Tseting-nil/Tower-Defense-Simulator/refs/heads/main/MapscriptURL/Four%20Seasons/Easy/Four_Seasons_1.lua"
+	},
+	{
+		Name = "嘻嘻哈哈 - 12",
+		Title = "Four Seasons (測試12)(Tseting-nil)",
+		Author = "Tseting-nil",
+		Time = "1111111",
+		Tower = {"Farm","GoldSoldier","Medic","DJ Booth","Accelerator"},--ACC = Accelerator
+		Script = "https://raw.githubusercontent.com/Tseting-nil/Tower-Defense-Simulator/refs/heads/main/MapscriptURL/Four%20Seasons/Easy/Four_Seasons_1.lua"
+	},
+	{
+		Name = "嘻嘻哈哈 - 13",
+		Title = "Four Seasons (測試13)(Tseting-nil)",
+		Author = "Tseting-nil",
+		Time = "1111111",
+		Tower = {"Farm","GoldSoldier","Medic","DJ Booth","Accelerator"},--ACC = Accelerator
+		Script = "https://raw.githubusercontent.com/Tseting-nil/Tower-Defense-Simulator/refs/heads/main/MapscriptURL/Four%20Seasons/Easy/Four_Seasons_1.lua"
+	},
 }
 return Script
