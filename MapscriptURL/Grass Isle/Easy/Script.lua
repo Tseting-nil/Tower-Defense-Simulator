bd97675ef7d09d326e6100be5c8c72fb2003851b
Tseting-nil <chenjustin399.5@gmail.com>
2025-08-01 11:39:51 +0800

    為地圖: Grass Isle 添加自動腳本庫

---
+++ MapscriptURL/Grass Isle/Easy/Script.lua
@@ -0,0 +1,11 @@
+local Script = {
+	{
+		Name = "  作者自建  ",
+		Title = " 需要加入遊戲群組 ",
+		Author = "Tseting-nil",
+		Time = "09:13",
+		Tower = {"Soldier","Pyromancer","Minigunner"},--ACC = Accelerator
+		Script = "https://raw.githubusercontent.com/Tseting-nil/Tower-Defense-Simulator/refs/heads/main/MapscriptURL/Grass%20Isle/Easy/Grass_Isle_1.lua"
+	},
+}
+return Script
