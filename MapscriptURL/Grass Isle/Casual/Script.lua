b754ecf564a6db81f2656a78775cc665828b961c
Tseting-nil <chenjustin399.5@gmail.com>
2025-08-05 22:08:59 +0800

    添加地圖腳本:Grasslsle_Casual

---
+++ MapscriptURL/Grass Isle/Casual/Script.lua
@@ -0,0 +1,11 @@
+local Script = {
+	{
+		Name = "  作者自建  ",
+		Title = " 需要加入遊戲群組 ",
+		Author = "Tseting-nil",
+		Time = "15:33",
+		Tower = {"Soldier","Pyromancer","Minigunner"},
+		Script = "https://raw.githubusercontent.com/Tseting-nil/Tower-Defense-Simulator/refs/heads/main/MapscriptURL/Grass%20Isle/Casual/Grass_Isle_1.lua"
+	},
+}
+return Script
